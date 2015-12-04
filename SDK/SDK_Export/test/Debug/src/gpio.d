b050c73be0beca711340f9be2e1a92b24cecc157
zhounaiding <zhounaiding@gmail.com>
2015-12-04 18:47:01 +0800

    ok

---
+++ SDK/SDK_Export/test/Debug/src/gpio.d
@@ -0,0 +1,40 @@
+src/gpio.d: ../src/gpio.c \
+ ../../standalone_bsp_0/microblaze_0/include/xparameters.h \
+ ../../standalone_bsp_0/microblaze_0/include/xgpio.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_types.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_assert.h \
+ ../../standalone_bsp_0/microblaze_0/include/xstatus.h \
+ ../../standalone_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../standalone_bsp_0/microblaze_0/include/xgpio_l.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_io.h \
+ ../../standalone_bsp_0/microblaze_0/include/mb_interface.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_printf.h \
+ ../../standalone_bsp_0/microblaze_0/include/xintc.h \
+ ../../standalone_bsp_0/microblaze_0/include/xparameters.h \
+ ../../standalone_bsp_0/microblaze_0/include/xintc_l.h
+
+../../standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+../../standalone_bsp_0/microblaze_0/include/xgpio.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_assert.h:
+
+../../standalone_bsp_0/microblaze_0/include/xstatus.h:
+
+../../standalone_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xgpio_l.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_io.h:
+
+../../standalone_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_printf.h:
+
+../../standalone_bsp_0/microblaze_0/include/xintc.h:
+
+../../standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+../../standalone_bsp_0/microblaze_0/include/xintc_l.h:
